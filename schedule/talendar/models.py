9424d4805205251e1d2ffac04f5e14cd0a4fc98c
David Gu <david.gu@hotmail.com>
2018-02-23 15:58:19 -0500

    Added event model

--- schedule/talendar/models.py
+++ schedule/talendar/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Event:
+    name = models.CharField(max_length=75)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
